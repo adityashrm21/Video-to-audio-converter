499f4e2fe96459b4572250bf5f5cb3483f8b7b19
Aditya Sharma <adityashrm21@gmail.com>
2016-05-24 17:18:24 +0530

    Changes added for files with any extension
    1. Now video files with any extensions can be converted into mp3 files
    2. File names with only no special characters are accepted as of now 
    3. To fix: Accept files with any name

--- video_to_audio.py
+++ video_to_audio.py
@@ -11,12 +11,12 @@ import os
 def video_to_audio(fileName):
 
 	try:
-		os.rename(fileName, 'audio.mp4')
-		video_to_wav = 'ffmpeg -i '+'audio.mp4'+' '+'audio.wav'
-		final_audio ='lame '+'audio.wav'+' '+'audio.mp3'
+		file,file_extension=os.path.splitext(fileName)
+		video_to_wav = 'ffmpeg -i '+file+file_extension+' '+file+'.wav'
+		final_audio ='lame '+file+'.wav'+' '+file+'.mp3'
 		os.system(video_to_wav)
 		os.system(final_audio)
-		os.remove('audio.wav')
+		os.remove(file+'.wav')
 		print("sucessfully converted ",fileName, " into audio!")
 	except OSError as err:
 		print(err.reason)
@@ -24,26 +24,26 @@ def video_to_audio(fileName):
 
 
 def main():
-    if len(sys.argv) <1 or len(sys.argv) > 2:
-        print('command usage: python video_to_audio.py FilePath')
-        exit(1)
-    else:
-        filePath = sys.argv[1]
-    
-        # check if the specified file exists or not
-        try:
-            if os.path.exists(filePath):
-            	print("file found!")
-        except OSError as err:
-            print(err.reason)
-            exit(1)
+	if len(sys.argv) <1 or len(sys.argv) > 2:
+		print('command usage: python video_to_audio.py FileName')
+		exit(1)
+	else:
+		filePath = sys.argv[1]
+	
+		# check if the specified file exists or not
+		try:
+			if os.path.exists(filePath):
+				print("file found!")
+		except OSError as err:
+			print(err.reason)
+			exit(1)
 
 
-        # convert video to audio
-        video_to_audio(filePath)
-        time.sleep(1)
+		# convert video to audio
+		video_to_audio(filePath)
+		time.sleep(1)
 
 # install ffmpeg and/or lame if you get an error saying that the program is currently not installed 
 
 if __name__ == '__main__':
-    main()
+	main()
