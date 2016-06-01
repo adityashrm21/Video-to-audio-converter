a72eb35c417910d343958c05eee9f62b75a914e1
Aditya Sharma <adityashrm21@gmail.com>
2016-06-01 16:18:34 +0530

    Changes added for accepting any filenames
    Now the script will accept any file name, but you have to delete the wav files by yourself

--- video_to_audio.py
+++ video_to_audio.py
@@ -12,15 +12,18 @@ def video_to_audio(fileName):
 
 	try:
 		file,file_extension=os.path.splitext(fileName)
+		file=pipes.quote(file)
 		video_to_wav = 'ffmpeg -i '+file+file_extension+' '+file+'.wav'
 		final_audio ='lame '+file+'.wav'+' '+file+'.mp3'
 		os.system(video_to_wav)
 		os.system(final_audio)
-		os.remove(file+'.wav')
+		#file=pipes.quote(file)
+		#os.remove(file+'.wav')
 		print("sucessfully converted ",fileName, " into audio!")
 	except OSError as err:
 		print(err.reason)
 		exit(1)
+
 
 
 def main():
