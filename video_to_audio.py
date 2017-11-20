ba2ba95fc81a2100af0868730a6686a2f45831d1
Aditya Sharma <adityashrm21@gmail.com>
2017-11-20 11:51:56 +0530

    minor formatting changes

--- video_to_audio.py
+++ video_to_audio.py
@@ -2,7 +2,6 @@
 
 import urllib.request
 import urllib.error
-
 import re
 import sys
 import time
@@ -10,7 +9,6 @@ import os
 import pipes
 
 def video_to_audio(fileName):
-
 	try:
 		file,file_extension=os.path.splitext(fileName)
 		file=pipes.quote(file)
@@ -25,15 +23,12 @@ def video_to_audio(fileName):
 		print(err.reason)
 		exit(1)
 
-
-
 def main():
 	if len(sys.argv) <1 or len(sys.argv) > 2:
 		print('command usage: python3 video_to_audio.py FileName')
 		exit(1)
 	else:
 		filePath = sys.argv[1]
-	
 		# check if the specified file exists or not
 		try:
 			if os.path.exists(filePath):
@@ -41,13 +36,10 @@ def main():
 		except OSError as err:
 			print(err.reason)
 			exit(1)
-
-
 		# convert video to audio
 		video_to_audio(filePath)
 		time.sleep(1)
-
+		
 # install ffmpeg and/or lame if you get an error saying that the program is currently not installed 
-
 if __name__ == '__main__':
 	main()
