2cc66281ba38a188130b6fc80f5641182dabe621
Aditya Sharma <adityashrm21@gmail.com>
2017-11-20 11:53:38 +0530

    minor formatting changes

--- video_to_audio.py
+++ video_to_audio.py
@@ -10,15 +10,15 @@ import pipes
 
 def video_to_audio(fileName):
 	try:
-		file,file_extension=os.path.splitext(fileName)
-		file=pipes.quote(file)
-		video_to_wav = 'ffmpeg -i '+file+file_extension+' '+file+'.wav'
-		final_audio ='lame '+file+'.wav'+' '+file+'.mp3'
+		file, file_extension = os.path.splitext(fileName)
+		file = pipes.quote(file)
+		video_to_wav = 'ffmpeg -i ' + file + file_extension + ' ' + file + '.wav'
+		final_audio = 'lame '+ file + '.wav' + ' ' + file + '.mp3'
 		os.system(video_to_wav)
 		os.system(final_audio)
 		#file=pipes.quote(file)
-		#os.remove(file+'.wav')
-		print("sucessfully converted ",fileName, " into audio!")
+		#os.remove(file + '.wav')
+		print("sucessfully converted ", fileName, " into audio!")
 	except OSError as err:
 		print(err.reason)
 		exit(1)
